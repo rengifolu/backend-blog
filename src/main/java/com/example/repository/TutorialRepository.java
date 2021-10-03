743238c269c98cfe3750c011b1fe48e4a0e1dcdf
rengifolu <andrelujan7@gmail.com>
2021-10-03 20:59:35 +0200

    primera version funcional con backend

---
+++ src/main/java/com/example/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.repository;
+
+import com.example.model.Tutorial;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
